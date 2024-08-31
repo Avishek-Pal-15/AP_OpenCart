236a99c203fce99a7beb62536eb0a82114d76e71
AvishekPal <a.pal473@gmail.com>
2024-08-31 21:26:22 +0530

    Added log4j2.xml and some reusable methods in TestBase

--- src/test/java/testBase/TestBase.java
+++ src/test/java/testBase/TestBase.java
@@ -1,18 +1,27 @@
 package testBase;
 
 import java.time.Duration;
+import java.util.Random;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class TestBase {
 
 	public WebDriver driver;
+	public Logger logger;
 	
 	@BeforeClass
 	public void setup() { 
+		BasicConfigurator.configure();
+		logger = LogManager.getLogger(this.getClass());
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://tutorialsninja.com/demo/");
@@ -23,4 +32,24 @@ public class TestBase {
 	public void tearDown() { 
 		driver.quit();
 	}
+	
+	public static void waitUntilPageLoad(WebDriver driver, int timeout) {
+		System.out.println("waiting for page load!");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+        wait.until(ExpectedConditions.jsReturnsValue("return document.readyState==\"complete\""));
+	}
+	
+	public static String generateRandomString(int minLength, int maxLength) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        int length = random.nextInt((maxLength - minLength) + 1) + minLength;
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            sb.append(characters.charAt(index));
+        }
+
+        return sb.toString();
+    }
 }
--- src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -2,10 +2,9 @@ package testCases;
 
 import java.time.Duration;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import pageObjects.AccountRegistrationPage;
@@ -19,21 +18,45 @@ public class TC001_AccountRegistrationTest extends TestBase {
 		@Test
 		void verifyRegistration() { 
 			
-			HomePage hp = new HomePage(driver);
-			hp.clickMyAccount();
-			hp.clickRegister();
+			try {
+				logger.info("************ Starting TC001 ****************");
+				
+				HomePage hp = new HomePage(driver);
+				hp.clickMyAccount();
+				hp.clickRegister();
+				
+				AccountRegistrationPage arp = new AccountRegistrationPage(driver);
+				
+				arp.setFN("Avi");
+				arp.setLN("Anu");
+				arp.setEmail(generateRandomString(5, 10) + "@gmail.com");
+				arp.setPH("1234567891");
+				arp.setPwd("abcd1234");
+				arp.setConfirm("abcd1234");
+				arp.selectRadio();
+				arp.agreeCheckbox();
+				arp.submit();
+				
+				//Thread.sleep(2000);
+				waitUntilPageLoad(driver, 5);
+		        
+				String confMsg = arp.getConfMsg(); 
+				//System.out.println(confMsg);
+				
+				if(confMsg.contains("successfully created")) {
+					Assert.assertTrue(true);
+				} else {
+					logger.error("Test Failed");
+					logger.debug("Debug logs");
+					Assert.assertTrue(false);
+				}
+				
+			} catch (Exception e) {	
+				Assert.fail();
+			}
 			
-			AccountRegistrationPage arp = new AccountRegistrationPage(driver);
+			logger.info("************ Submitted form. End of TC001 ****************");
 			
-			arp.setFN("Avi");
-			arp.setLN("Anu");
-			arp.setEmail("abc@gmail.com");
-			arp.setPH("1234567891");
-			arp.setPwd("abcd1234");
-			arp.setConfirm("abcd1234");
-			arp.selectRadio();
-			arp.agreeCheckbox();
-			arp.submit();
 		}
 		
 		
